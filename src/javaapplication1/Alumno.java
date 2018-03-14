b997175ee4bdd97d373434796413d2c679fd5e04
Ricardo-1998 <37321338+Ricardo-1998@users.noreply.github.com>
2018-03-14 12:22:27 -0600

    Add files via upload

---
+++ src/javaapplication1/Alumno.java
@@ -0,0 +1,24 @@
+package javaapplication1;
+
+public class Alumno {
+
+    String nombre;
+    String carne;
+    String Dui;
+    int edad;
+    int phone;
+    String hobbie;
+    
+   
+    
+    public void estudiante(String nombre, String carne, String Dui, int edad, int phone, String hobbie) {
+        System.out.println("nombre: " + nombre);
+        System.out.println("carne: " + carne);
+        System.out.println("Dui: " + Dui);
+        System.out.println("edad: " + edad);
+        System.out.println("phone: " + phone);
+        System.out.println("hobbie: " + hobbie);
+
+    }
+
+}
---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author Ricardo Villeda
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Alumno alumno1= new Alumno();
+  
+        alumno1.estudiante("Ricardo Antonio Villeda Flores", "00004817", "057156-0", 19, 76610681, "Jugar LoL");
+        
+      
+    }
+    
+    
+    
+}
